08fe4e5572ed738292315488fd5ee1b3f0c5d31c
Srinag S <srinagrao2007@gmail.com>
2018-06-12 10:57:06 +0530

    Publishes everything as std_msgs/String

--- mudboxer.cpp
+++ mudboxer.cpp
@@ -12,6 +12,7 @@
 #include <arpa/inet.h>
 #include "ros/ros.h"
 #include "Crypt.h"
+#include "std_msgs/String.h"
 
 #define AS_PORT 8000
 bool called_init = false;
@@ -32,7 +33,7 @@ void init_mudboxer() {
     myCrypt.load_private_key(getenv("ROSS_NODE_KEY"), getenv("ROSS_NODE_PASS"));
     myCrypt.load_my_cert(getenv("ROSS_NODE_CERT"), true);
     client.init();
-    if (!client.connect("AuthServer", "localhost", 8000)) {
+    if (!client.connect("AuthServer", "localhost", AS_PORT)) {
         printf("Couldn't connect to AuthServer\n");
         term_mudboxer();
         exit(EXIT_FAILURE);
@@ -49,12 +50,19 @@ void term_mudboxer() {
     printf("%s\n", "\nBYE!");
 }
 
-void myAdvert(ros::AdvertiseOptions &ops) {
-    printf("ADVERTISING %s %s\n", ops.topic.c_str(), ops.datatype.c_str());
+void mod_pub_ops(ros::AdvertiseOptions &ops) {
     std::stringstream msg;
-    msg << "Hello AS";
+    msg << "REG_PUB;" << ops.topic.c_str() << ";" << ops.datatype.c_str();
     client.write(msg.str());
+    std::string key;
+    client.read(key);
+    myCrypt.save_aes_key(ops.topic, key);
+    ops.datatype = ros::message_traits::DataType<std_msgs::String>::value();
+    ops.md5sum = ros::message_traits::MD5Sum<std_msgs::String>::value();
+    ops.message_definition = ros::message_traits::definition<std_msgs::String>();
+    ops.has_header = ros::message_traits::hasHeader<std_msgs::String>();
 }
+
 
 namespace ros {
     void init(int &argc, char **argv, const std::string &name, uint32_t options) {
@@ -71,17 +79,32 @@ namespace ros {
         ptr();
     }
 
+    void Publisher::publish(const boost::function<SerializedMessage(void)> &serfunc, SerializedMessage &m) const {
+//        m.type_info = &typeid(std_msgs::String);
+        printf("%zu %s\n", m.num_bytes, (char *)m.buf.get());
+        
+        typedef void (*type)(const Publisher *, const boost::function<SerializedMessage(void)> &,
+                             ros::SerializedMessage &);
+        auto ptr = (type) dlsym(RTLD_NEXT,
+                                "_ZNK3ros9Publisher7publishERKN5boost8functionIFNS_17SerializedMessageEvEEERS3_");
+        return ptr(this, serfunc, m);
+    }
+
     Publisher NodeHandle::advertise(AdvertiseOptions &ops) {
-        myAdvert(ops);
+        mod_pub_ops(ops);
         typedef Publisher (*type)(NodeHandle *, AdvertiseOptions &);
         auto ptr = (type) dlsym(RTLD_NEXT, "_ZN3ros10NodeHandle9advertiseERNS_16AdvertiseOptionsE");
-        return ptr(this, ops);
+        auto pub_er = ptr(this, ops);
+        // Bypass ROS_ASSERT_MSG at publisher.h line 79
+        pub_er.impl_->md5sum_ = "*";
+        return pub_er;
     }
 
     Subscriber NodeHandle::subscribe(SubscribeOptions &ops) {
-        printf("SUBSCRIBING to %s\n", ops.topic.c_str());
+//        mySub(ops);
         typedef Subscriber (*type)(NodeHandle *, SubscribeOptions &);
         auto ptr = (type) dlsym(RTLD_NEXT, "_ZN3ros10NodeHandle9subscribeERNS_16SubscribeOptionsE");
         return ptr(this, ops);
     }
+
 } // namespace ros
