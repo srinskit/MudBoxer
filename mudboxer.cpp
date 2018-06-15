904355266a22a9cda15b0ea30ed6711497038307
Srinag S <srinagrao2007@gmail.com>
2018-06-15 11:00:46 +0530

    now comes with sub registration and AES encrypted comm

--- mudboxer.cpp
+++ mudboxer.cpp
@@ -7,18 +7,15 @@
 #include <string>
 #include <cstdio>
 #include <cstdlib>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include "ros/ros.h"
 #include "Crypt.h"
 #include "std_msgs/String.h"
-#include "geometry_msgs/Twist.h"
-#include "ros/subscription_queue.h"
 #include "ros/message_deserializer.h"
 
 #define AS_PORT 8000
-bool called_init = false;
+
+typedef std_msgs::String ROS_STR;
+bool called_init = false, enc = true;
 std::string source_name, node_name;
 Crypt myCrypt;
 SecureSock::Client client(&myCrypt);
@@ -29,7 +26,7 @@ void term_mudboxer();
 void init_mudboxer() {
     if (called_init) return;
     called_init = true;
-    printf("%s\n", "RUNNING INSIDE WRAPPER");
+    printf("\n%s\n", "--- MUDBOXER ---");
     myCrypt.initialize("ROSS");
     source_name = getenv("ROSS_SOURCE_NAME");
     node_name = getenv("ROSS_NODE_NAME");
@@ -38,7 +35,7 @@ void init_mudboxer() {
     myCrypt.load_my_cert(getenv("ROSS_NODE_CERT"), true);
     client.init();
     if (!client.connect("AuthServer", "localhost", AS_PORT)) {
-        printf("Couldn't connect to AuthServer\n");
+        printf("Could not connect to AuthServer :(\n");
         term_mudboxer();
         exit(EXIT_FAILURE);
     }
@@ -51,28 +48,50 @@ void term_mudboxer() {
     client.close();
     client.terminate();
     myCrypt.terminate();
-    printf("%s\n", "\nBYE!");
+    printf("%s\n", "\n---   BYE!   ---");
 }
-
-typedef std_msgs::String ROS_STR;
 
 
 void mod_msg(const boost::function<ros::SerializedMessage(void)> &serfunc,
              boost::function<ros::SerializedMessage(void)> &new_serfunc, ROS_STR &new_msg) {
     ros::SerializedMessage m2 = serfunc();
-    new_msg.data.append(reinterpret_cast<char *>(m2.buf.get()), m2.num_bytes);
+    std::string payload(reinterpret_cast<char *>(m2.buf.get()), m2.num_bytes);
+    auto topic = new_msg.data.substr(0, new_msg.data.length() - 1);
+    if (enc) {
+        std::string tmp;
+        myCrypt.aes_encrypt(payload, topic, tmp);
+        payload = tmp;
+    }
+    new_msg.data.append(payload);
     new_serfunc = boost::bind(ros::serialization::serializeMessage<ROS_STR>, boost::ref(new_msg));
 }
 
-void unmod_msg(const boost::shared_ptr<ROS_STR const> &new_msg) {
+void unmod_msg(const boost::shared_ptr<ROS_STR const> &new_msg, ros::SerializedMessage &m, std::string &topic) {
     std::string str = new_msg->data;
     auto colon_pos = str.find_first_of(';');
-    auto topic = str.substr(0, colon_pos);
-    ros::SerializedMessage m;
+    topic = str.substr(0, colon_pos);
+    if (enc) {
+        std::string tmp;
+        myCrypt.aes_decrypt(str.substr(colon_pos + 1), topic, tmp);
+        str = topic + ';' + tmp;
+    }
     m.num_bytes = new_msg->data.length() - colon_pos - 1;
     m.buf.reset(new uint8_t[m.num_bytes]);
     str.copy(reinterpret_cast<char *>(m.buf.get()), m.num_bytes, colon_pos + 1);
     m.message_start = m.buf.get() + 4;
+}
+
+std::string standardize_topic(const std::string &topic) {
+    if (topic.length() == 0)return "";
+    if (topic[0] != '/') return '/' + topic;
+    return topic;
+}
+
+
+void deliver_msg(const boost::shared_ptr<ROS_STR const> &str) {
+    ros::SerializedMessage m;
+    std::string topic;
+    unmod_msg(str, m, topic);
     auto connection_header = boost::make_shared<ros::M_string>();
     ros::SubscriptionCallbackHelperCallParams params;
     for (auto &helper : real_helpers[topic]) {
@@ -84,30 +103,34 @@ void unmod_msg(const boost::shared_ptr<ROS_STR const> &new_msg) {
     }
 }
 
-void mod_pub_ops(ros::AdvertiseOptions &ops) {
+bool mod_pub_ops(ros::AdvertiseOptions &ops) {
+    auto topic = standardize_topic(ops.topic);
+    if (topic.empty())
+        return false;
     std::stringstream msg;
-    msg << "REG_PUB;" << ops.topic.c_str() << ";" << ops.datatype.c_str();
+    msg << "REG_PUB;" << topic;
     client.write(msg.str());
     std::string key;
     client.read(key);
-    myCrypt.save_aes_key(ops.topic, key);
-    auto topic = ops.topic;
+    myCrypt.aes_save_key(topic, key);
     auto qs = ops.queue_size;
     ops.template init<ROS_STR>(topic, qs);
 }
 
-
-void trans_callback(const boost::shared_ptr<ROS_STR const> &str) {
-    unmod_msg(str);
-}
-
-void mod_sub_ops(ros::SubscribeOptions &ops) {
-    auto topic = ops.topic;
-    if (topic[0] != '/') topic = '/' + topic;
+bool mod_sub_ops(ros::SubscribeOptions &ops) {
+    auto topic = standardize_topic(ops.topic);
+    if (topic.empty())
+        return false;
+    std::stringstream msg;
+    msg << "REG_SUB;" << topic;
+    client.write(msg.str());
+    std::string key;
+    client.read(key);
+    myCrypt.aes_save_key(topic, key);
     real_helpers[topic].push_back(ops.helper);
     auto qs = ops.queue_size;
     ops.helper.reset();
-    ops.template init<ROS_STR>(topic, qs, trans_callback);
+    ops.template init<ROS_STR>(topic, qs, deliver_msg);
 }
 
 namespace ros {
